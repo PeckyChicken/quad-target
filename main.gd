abf520ae2f38691b9c710873ae8efda5bbca1365
Benjy <BenjyPeckChicken@gmail.com>
2025-12-13 11:31:49 +0000

    FEATURE: GitHub button appears in the corner for reporting bugs

--- main.gd
+++ main.gd
@@ -47,6 +47,7 @@ var tile_scale = Vector2.ONE
 const WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const MONTHS = ["January", "February", "March", "April", "May", "June",
 			  "July", "August", "September", "October", "November", "December"]
+const GITHUB_LINK = &"https://github.com/PeckyChicken/quad-target/issues"
 
 var __ := 0
 
@@ -324,3 +325,10 @@ func _input(_event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	timer += delta
+
+
+func _on_github_pressed() -> void:
+	if OS.has_feature("web"):
+		JavaScriptBridge.eval(GITHUB_LINK)
+	else:
+		OS.shell_open(GITHUB_LINK)
