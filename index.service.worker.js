2ef8d6a64ab556a5a27a1092d83fdacf38201bcc
Benjy <BenjyPeckChicken@gmail.com>
2025-12-13 11:40:06 +0000

    BUG FIX: GitHub link now opens in browser

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765625483|4882798142';
+const CACHE_VERSION = '1765625995|5394635036';
 /** @type {string} */
 const CACHE_PREFIX = 'Quad Target-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
