d7d62e5b47d5529b217758ee9379026db36c508c
Benjy <BenjyPeckChicken@gmail.com>
2026-05-09 11:14:26 +0100

    CLEANUP: Removed unnecessary references to SharePlugin

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773767211|92970103272';
+const CACHE_VERSION = '1778321640|1751264586';
 /** @type {string} */
 const CACHE_PREFIX = 'Quad Target-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
